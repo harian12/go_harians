e40d7384206b98969ddf8845fbef16b0a7c9537c
harian12 <hariansyah434@gmail.com>
2022-06-13 22:55:04 +0700

    cek jens argument

--- command/argument.go
+++ command/argument.go
@@ -1,1 +1,16 @@
 package command
+
+import (
+	"os"
+)
+
+func Arguments() {
+	if len(os.Args) == 2 || len(os.Args) == 3 {
+		argument := os.Args[1:]
+		if argument[0] == Migrate {
+			ArgMigrate()
+		} else if argument[0] == Seeder {
+			ArgSeeder()
+		}
+	}
+}
