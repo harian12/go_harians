fb332a31965848a6e3cd38856dccd1e9ceae5566
harian12 <hariansyah434@gmail.com>
2022-06-13 22:54:30 +0700

    call argument commond

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"go_harians/command"
 	"go_harians/config"
 	"go_harians/router"
 )
 
 func main() {
 	config.COnnectionDB()
+	command.Arguments()
 	router.AppRouter()
 }
