49edbc46d7ac4e09f325c3f2e8813d1d31fc41f0
harian12 <hariansyah434@gmail.com>
2022-06-18 15:07:32 +0700

    description APP ENV

--- router/AppRouter.go
+++ router/AppRouter.go
@@ -2,10 +2,23 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"go_harians/config"
 )
 
+type AppConfig struct {
+	AppName string
+	AppEnv  string
+	AppPort string
+}
+
 func AppRouter() {
+	var appConfig = AppConfig{}
+
+	appConfig.AppName = config.GetEnv("APP_NAME", "Harians")
+	appConfig.AppEnv = config.GetEnv("APP_ENV", "development")
+	appConfig.AppPort = config.GetEnv("APP_PORT", "8080")
+
 	r := gin.Default()
 	RouterV1(r)
-	r.Run()
+	r.Run(":" + appConfig.AppPort)
 }
