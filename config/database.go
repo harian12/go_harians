04040556905ad4156a6ef5cc2f8930f4516f24ea
harian12 <hariansyah434@gmail.com>
2022-06-19 22:45:27 +0700

    pemindahan fungsi getenv ke helpers

--- config/database.go
+++ config/database.go
@@ -2,13 +2,20 @@ package config
 
 import (
 	"fmt"
+	"go_harians/app/helpers"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-func COnnectionDB(dbConfig DBConfig) *gorm.DB {
+func COnnectionDB() *gorm.DB {
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbConfig.DB_USER, dbConfig.DB_PASS, dbConfig.DB_HOST, dbConfig.DB_PORT, dbConfig.DB_NAME)
+	DB_HOST := helpers.GetEnv("DB_HOST", "localhost")
+	DB_PORT := helpers.GetEnv("DB_PORT", "3306")
+	DB_USER := helpers.GetEnv("DB_USER", "root")
+	DB_PASS := helpers.GetEnv("DB_PASS", "sky")
+	DB_NAME := helpers.GetEnv("DB_NAME", "harians")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
 	db, errDB := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if errDB != nil {
@@ -17,5 +24,4 @@ func COnnectionDB(dbConfig DBConfig) *gorm.DB {
 	//command.NewMigrate(db)
 	//seeders.DBSeed(db)
 	return db
-
 }
